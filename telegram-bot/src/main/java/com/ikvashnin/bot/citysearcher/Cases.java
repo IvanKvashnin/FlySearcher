b36c6bc36df8c0b0e363327036b2b2572fde3bc8
ikvashnin <i89998004607@gmail.com>
2023-01-14 17:26:04 +0300

    FlySearcher_#4 add Date and City pareser

--- telegram-bot/src/main/java/com/ikvashnin/bot/citysearcher/Cases.java
+++ telegram-bot/src/main/java/com/ikvashnin/bot/citysearcher/Cases.java
@@ -1,8 +1,10 @@
 package com.ikvashnin.bot.citysearcher;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Cases {
     String su;
     String da;
--- telegram-bot/src/main/java/com/ikvashnin/bot/citysearcher/City.java
+++ telegram-bot/src/main/java/com/ikvashnin/bot/citysearcher/City.java
@@ -2,13 +2,21 @@ package com.ikvashnin.bot.citysearcher;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 public class City {
     String code;
     String name;
     Cases cases;
+
+    public boolean isCityName(String name) {
+        List<String> names = List.of(this.name, cases.da, cases.pr, cases.ro, cases.su, cases.tv, cases.vi);
+        return names.contains(name);
+    }
 }
